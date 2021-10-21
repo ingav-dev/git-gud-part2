718aa654299a9a56be36e53ebf7716c04f0e545e
ingav-dev <inga.velescu@gmail.com>
2021-10-21 12:04:44 -0600

    docs: changed the error in docstring

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def multiply(num1:int, num2:int, num3:int) -> int:
     return num1 * num2 * num3
 
 def add(num1:int, num2:int) -> int:
-    ''' Add aaddition function '''
+    ''' Add addition function '''
     return num1 + num2
