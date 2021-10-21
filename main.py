875fdea0621f55986c41fe0b2cda8b192666c49b
Xiaoyu Sun <sunxy@Xiaoyus-MacBook-Air.local>
2021-10-21 16:53:15 -0600

    feat: adds subtraction function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def multiply(num1:int, num2:int, num3:int) -> int:
+    ''' Add multiplication function '''
     return num1 * num2 * num3
 
 
@@ -6,3 +7,6 @@ def add(num1:int, num2:int) -> int:
     ''' Add addition function '''
     return num1 + num2
 
+def subtract(num1:int, num2:int) -> int:
+    ''' Add subtraction function '''
+    return num1 - num2
