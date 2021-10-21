bcf658c5b8addbbe5c241a760be820d465ab1b60
ingav-dev <inga.velescu@gmail.com>
2021-10-21 10:54:42 -0600

    docs: add a doctring for addition function

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 def multiply(num1:int, num2:int, num3:int) -> int:
-    return num1 * num2 * int3
+    return num1 * num2 * num3
+
+def add(num1:int, num2:int) -> int:
+    ''' Add aaddition function '''
+    return num1 + num2
